4c9c5f68d83c26275f4dddd117d71a5cc0c32bd1
bob <1478449280@qq.com>
2021-12-09 16:55:11 +0800

    update star_notify.py

--- .github/workflows/star_notify.py
+++ .github/workflows/star_notify.py
@@ -16,14 +16,25 @@ if __name__ == '__main__':
             'Authorization': 'Bearer ' + token,
         }
     ).json()['data']
-    last_user = data['repository']['stargazers']['edges'][0]['node']
+
+    try:
+        last_user = data['repository']['stargazers']['edges'][0]['node']
+        content = f"""
+<div style="text-align: center;">
+    <h1>{data['repository']['stargazerCount']} 💕</h1>
+    <img src="{last_user['avatarUrl']}" alt="avatar" style="width:200px; border-radius: 100px">
+    <div style="margin: 10px; font-size: x-large">{last_user['name']} {last_user['email']}</div>
+    <a href="{last_user['url']}" style="display: block; font-size: large">{last_user['url']}</a>
+</div>
+"""
+    except:
+        content = f"""
+<div style="text-align: center;">
+    <h5>刚刚有个吊毛，给你标星，又取消了 💕</h5>
+    <h3>不知道是谁 😂</h3>
+    <h4>是不是你自己呀 🤣</h4>
+</div>
+"""
     yag = yagmail.SMTP({'started_notify@163.com': 'started notify'}, 'QDCONGQTZIIZKBBI', 'smtp.163.com', 465)
     notify_email = sys.argv[2]
-    yag.send(notify_email, f'[{user}/{repo}] Started', f"""
-        <div style="text-align: center;">
-            <h1>{data['repository']['stargazerCount']} 💕</h1>
-            <img src="{last_user['avatarUrl']}" alt="avatar" style="width:200px; border-radius: 100px">
-            <div style="margin: 10px; font-size: x-large">{last_user['name']} {last_user['email']}</div>
-            <a href="{last_user['url']}" style="display: block; font-size: large">{last_user['url']}</a>
-        </div>
-    """)
+    yag.send(notify_email, f'[{user}/{repo}] Started', content)
